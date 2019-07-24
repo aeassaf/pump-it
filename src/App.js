b47141a69d38c92bca5d5563ac8df2773df16811
Antoine-Assaf15 <eliasassaf.antoine@gmail.com>
2019-07-24 12:39:03 +0300

    feat: react-router

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Home from './components/Home'
+import Garage from './components/Garage'
+import gas_stations from './components/gas-stations'
+import paint_shops from './components/paint-shops'
+import Navigation from './components/Navigation'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Navigation />
+        <Switch>
+          <Route path="/" component={Home} exact />
+          <Route path="/paint-shops" component={paint_shops} />
+          <Route path="/maintenance" component={Garage} />
+          <Route path="/gas" component={gas_stations} />
+        </Switch>
+      </Router>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
