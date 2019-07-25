9df3ea9e03c80e67d3d40cac5e0f07e2f051f938
Antoine-Assaf15 <eliasassaf.antoine@gmail.com>
2019-07-25 12:27:57 +0300

    feat: add js code to gas page

---
+++ src/components/Gas-stations/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './index.css'
+
+const gas_stations = () => {
+  return (
+    <div className="pagestyle_gas">
+      <p>gas stations</p>
+    </div>
+  )
+}
+
+export default gas_stations
