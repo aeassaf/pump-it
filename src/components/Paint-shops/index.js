51b30470ee36ed7fffbb7a90a7caa8c99fcac070
Antoine-Assaf15 <eliasassaf.antoine@gmail.com>
2019-07-25 12:33:40 +0300

    feat: add design to page from css file

---
+++ src/components/Paint-shops/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './index.css'
+
+const PaintShops = () => {
+  return (
+    <div className="pagestyle_paint">
+      <p>paint shops</p>
+    </div>
+  )
+}
+
+export default PaintShops
