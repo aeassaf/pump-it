782364cf8f5dbfea036dbc6fedec5b193718dd57
Antoine Assaf <52399476+Antoine-Assaf15@users.noreply.github.com>
2019-07-25 18:17:31 +0300

    Design implementation
    Design implementation

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './components/Home';
+import Garage from './components/Garage';
+import gasStations from './components/Gas-stations';
+import PaintShops from './components/Paint-shops';
+import Navigation from './components/Navigation';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Navigation />
+        <Switch>
+          <Route path="/" component={Home} exact />
+          <Route path="/paint-shops" component={PaintShops} />
+          <Route path="/maintenance" component={Garage} />
+          <Route path="/gas" component={gasStations} />
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Paint-shops/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './index.css';
+
+const PaintShops = () => (
+  <div className="pagestyle_paint">
+    <p>paint shops</p>
+  </div>
+);
+
+export default PaintShops;
