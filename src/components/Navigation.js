056ca0259a7a60dbf8101a988a4acf628f08f2b2
Antoine-Assaf15 <eliasassaf.antoine@gmail.com>
2019-07-25 12:30:37 +0300

    feat: add navigation bar to project

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,14 +1,50 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom'
+import { Nav, Navbar } from 'react-bootstrap'
+import styled from 'styled-components'
 
+const Styles = styled.div`
+  .navbar {
+    background-color: #222;
+  }
+  .navbar-brand {
+    color: #7f00ff;
+  }
+
+  .navbar-brand:hover {
+    color: yellow;
+  }
+  .navbar-nav .nav-link {
+    color: #bbb;
+
+    &:hover {
+      color: white;
+    }
+  }
+`
 const Navigation = () => {
   return (
-    <div>
-      <NavLink to="/">Home </NavLink>
-      <NavLink to="/gas">gas </NavLink>
-      <NavLink to="/maintenance">maintenance </NavLink>
-      <NavLink to="/paint-shops">paint-shops </NavLink>
-    </div>
+    <Styles>
+      <Navbar expand="lg">
+        <Navbar.Brand href="/">Pump It </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />>
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Item>
+              <Nav.Link href="/">Home </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link href="/gas">gas </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link href="/maintenance">maintenance </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link href="/paint-shops">paint-shops </Nav.Link>
+            </Nav.Item>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </Styles>
   )
 }
 
