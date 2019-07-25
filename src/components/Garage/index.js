04932866ed3d3d64e402aa8edb330bd9b17b20c3
Antoine-Assaf15 <eliasassaf.antoine@gmail.com>
2019-07-25 14:35:44 +0300

    fix: adjust className

--- src/components/Garage/index.js
+++ src/components/Garage/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import './index.css'
 
 const Garage = () => {
-  return <div className="pagestyle" />
+  return <div className="pagestyle_garage" />
 }
 
 export default Garage
