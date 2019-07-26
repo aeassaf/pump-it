955a99abeb73f0d8040bf59e8cc6e8f1a280be80
Antoine-Assaf15 <eliasassaf.antoine@gmail.com>
2019-07-26 16:09:51 +0300

    feat: add flex to the Links

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -3,13 +3,13 @@ import './index.css';
 import { withRouter, Link } from 'react-router-dom';
 
 const Home = () => (
-  <div className="background">
+  <div className="background flex_container">
     <Link
       to="/gas"
-      className="buttonstyle_gas center_right btn btn-danger btn-lg"
+      className="buttonstyle flex_item btn btn-danger btn-lg"
     >
         Nearest Gas Station
-      <p align="center" className=" center_btn">
+      <p align="center" className=" flex_item_description">
           Find the nearest gas station
         {' '}
         <br />
@@ -20,10 +20,10 @@ to your location
 
     <Link
       to="/maintenance"
-      className="buttonstyle center btn btn-info btn-lg "
+      className="buttonstyle flex_item btn btn-info btn-lg"
     >
         Maintenance
-      <p align="center" className=" center_btn">
+      <p align="center" className="flex_item_description">
           Find the nearest garage
         {' '}
         <br />
@@ -34,10 +34,10 @@ to your location
 
     <Link
       to="/paint-shops"
-      className="buttonstyle_paint center_left btn btn-success btn-lg"
+      className="buttonstyle flex_item btn btn-success btn-lg"
     >
         Paint Shops
-      <p align="center" className=" center_btn">
+      <p align="center" className="flex_item_description">
           Find the nearest paint shop
         {' '}
         <br />
