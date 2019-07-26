99d2a43d95a321656b9c952d5f41cde31e4deb46
Antoine Assaf <52399476+Antoine-Assaf15@users.noreply.github.com>
2019-07-26 17:46:51 +0300

    feat: making home buttons design more responsive (#2)
    * feat: add flex to the Links
    
    * chore: made height instead of padding for links so they appear equal in height
    
    * feat: change font style and family and size, made the title of the button bigger than its description and gave the buttons rgba colors (#3)

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -2,14 +2,15 @@ import React from 'react';
 import './index.css';
 import { withRouter, Link } from 'react-router-dom';
 
+
 const Home = () => (
-  <div className="background">
+  <div className="background flex_container">
     <Link
       to="/gas"
-      className="buttonstyle_gas center_right btn btn-danger btn-lg"
+      className="buttonstyle btn btn-danger btn-lg red"
     >
-        Nearest Gas Station
-      <p align="center" className=" center_btn">
+    Nearest Gas Station
+      <p align="center" className=" flex_item_description smaller">
           Find the nearest gas station
         {' '}
         <br />
@@ -20,10 +21,10 @@ to your location
 
     <Link
       to="/maintenance"
-      className="buttonstyle center btn btn-info btn-lg "
+      className="buttonstyle btn btn-info btn-lg blue"
     >
-        Maintenance
-      <p align="center" className=" center_btn">
+      Maintenance
+      <p align="center" className="flex_item_description smaller">
           Find the nearest garage
         {' '}
         <br />
@@ -34,10 +35,10 @@ to your location
 
     <Link
       to="/paint-shops"
-      className="buttonstyle_paint center_left btn btn-success btn-lg"
+      className="buttonstyle btn btn-success btn-lg green"
     >
         Paint Shops
-      <p align="center" className=" center_btn">
+      <p align="center" className="flex_item_description smaller">
           Find the nearest paint shop
         {' '}
         <br />
