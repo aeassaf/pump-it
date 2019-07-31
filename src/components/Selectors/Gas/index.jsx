fca025143f9b793d15a91d048666d13656fd09b9
Antoine-Assaf15 <eliasassaf.antoine@gmail.com>
2019-07-31 15:54:19 +0300

    feat: dropdown values are now converted to geolocations

--- src/components/Selectors/Gas/index.jsx
+++ src/components/Selectors/Gas/index.jsx
@@ -1,14 +1,54 @@
 import React from 'react';
 import './index.css';
 
-const TypeofGas = () => (
-  <div>
-    <select className="select-css-gas">
-      <option value="none">Select</option>
-      <option value="Refuel">Refuel</option>
-      <option value="Wash">Car Wash</option>
-    </select>
-  </div>
-);
+import Geocode from 'react-geocode';
+
+require('dotenv').config();
+
+const { REACT_APP_GEOLOCATION_API } = process.env;
+Geocode.setApiKey(REACT_APP_GEOLOCATION_API);
+
+
+class TypeofGas extends React.Component {
+    state= {
+    }
+
+ location = (string) => {
+   Geocode.fromAddress(`Nearest ${string}`).then(
+     (response) => {
+       const { lat, lng } = response.results[0].geometry.location;
+       console.log(lat, lng);
+       return { lat, lng };
+     },
+
+     (error) => {
+       console.error(error);
+     },
+   );
+ }
+
+onClick = (event) => {
+  const { value } = event.target;
+  console.log(value);
+  if (value !== 'none' && value !== 'Other') {
+    const loco = this.location(value);
+  }
+}
+
+render() {
+  console.log(this.onClick);
+  console.log(this.showCoordinates);
+
+  return (
+    <div>
+      <select onChange={this.onClick} className="select-css-gas">
+        <option value="none">Select</option>
+        <option value="Refuel">Refuel</option>
+        <option value="Wash">Car Wash</option>
+      </select>
+    </div>
+  );
+}
+}
 
 export default TypeofGas;
--- src/components/Selectors/Type Of Paint/index.jsx
+++ src/components/Selectors/Type Of Paint/index.jsx
@@ -1,17 +1,56 @@
 import React from 'react';
 import './index.css';
+import Geocode from 'react-geocode';
 
-const TypeofPaint = () => (
-  <div>
-    <select className="select-css-paint">
-      <option value="none">Request</option>
-      <option value="Body Repair">Body Repair</option>
-      <option value="Stickers">Stickers</option>
-      <option value="Interior">Interior Design Customisations</option>
-      <option value="Wrap">Wrap</option>
-      <option value="Other">Other</option>
-    </select>
-  </div>
-);
+require('dotenv').config();
+
+const { REACT_APP_GEOLOCATION_API } = process.env;
+Geocode.setApiKey(REACT_APP_GEOLOCATION_API);
+
+
+class TypeofPaint extends React.Component {
+  state= {
+  }
+
+ location = (string) => {
+   Geocode.fromAddress(`Nearest ${string}`).then(
+     (response) => {
+       const { lat, lng } = response.results[0].geometry.location;
+       console.log(lat, lng);
+       return { lat, lng };
+     },
+
+     (error) => {
+       console.error(error);
+     },
+   );
+ }
+
+onClick = (event) => {
+  const { value } = event.target;
+  console.log(value);
+  if (value !== 'none' && value !== 'Other') {
+    const loco = this.location(value);
+  }
+}
+
+render() {
+  console.log(this.onClick);
+  console.log(this.showCoordinates);
+
+  return (
+    <div>
+      <select onChange={this.onClick} className="select-css-paint">
+        <option value="None">Request</option>
+        <option value="Body Repair">Body Repair</option>
+        <option value="Stickers">Stickers</option>
+        <option value="Interior">Interior Design Customisations</option>
+        <option value="Wrap">Wrap</option>
+        <option value="Other">Other</option>
+      </select>
+    </div>
+  );
+}
+}
 
 export default TypeofPaint;
