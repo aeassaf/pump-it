f88b947e2dbc99a26126777e4ae7f0ac365c0014
Antoine-Assaf15 <eliasassaf.antoine@gmail.com>
2019-08-06 14:30:19 +0300

    refactor: dropdown

--- src/components/Dropdown/index.jsx
+++ src/components/Dropdown/index.jsx
@@ -1,54 +1,22 @@
-import React, { createContext } from 'react';
-import { brandContent } from './Brand';
-
-export const dropDownContent = createContext();
+import React from 'react';
 
 class Dropdown extends React.Component {
-  static contextType = brandContent;
-
   static globalBrand;
 
     state= {
-      current_value: '',
-      current_brand: '',
     }
 
-     currentValue = (event) => {
-       const { value } = event.target;
+    render() {
+      const { content, currentValue } = this.props;
+      return (
+        <div>
+          <select onChange={currentValue}>
+            {content.map((val, index) => <option value={val} key={index}>{val}</option>)}
+          </select>
+        </div>
 
-       this.setState({ current_value: value });
-
-       if (this.globalBrand) {
-         this.setState({ current_brand: this.globalBrand });
-       }
-
-
-       console.log(this.state.current_value);
-     };
-
-
-     render() {
-       const { content } = this.props;
-       try {
-         const { current_brand } = this.context;
-         this.globalBrand = current_brand;
-       } catch (e) {
-         console.error(e);
-       }
-
-       console.log(content);
-       return (
-         <dropDownContent.Provider value={{ ...this.state }}>
-           <div>
-             <select onChange={this.currentValue}>
-               {content.map((val, index) => <option value={val} key={index}>{val}</option>)}
-               {console.log(this.state)}
-             </select>
-           </div>
-           {this.props.children}
-         </dropDownContent.Provider>
-       );
-     }
+      );
+    }
 }
 
 export default Dropdown;
