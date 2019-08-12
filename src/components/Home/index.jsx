5951fd7073fc53ee7fd9f95e1800f7c0f2c2277a
Antoine-Assaf15 <eliasassaf.antoine@gmail.com>
2019-08-12 12:32:17 +0300

    feat: media queries

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -2,16 +2,12 @@ import React from 'react';
 import './index.css';
 import { withRouter, Link } from 'react-router-dom';
 
-
 const Home = () => (
-  <div className="background flex_container">
-    <Link
-      to="/gas"
-      className="buttonstyle btn btn-danger btn-lg red"
-    >
-    Nearest Gas Station
-      <p align="center" className=" flex_item_description smaller">
-          Find the nearest gas station
+  <div className="backgroundHome flexContainer">
+    <Link to="/gas" className="buttonstyle btn btn-danger btn-lg red">
+      Gas Station
+      <p align="center" className=" flexItemDescription smaller">
+        Find the nearest gas station
         {' '}
         <br />
         {' '}
@@ -19,13 +15,10 @@ to your location
       </p>
     </Link>
 
-    <Link
-      to="/maintenance"
-      className="buttonstyle btn btn-info btn-lg blue"
-    >
+    <Link to="/maintenance" className="buttonstyle btn btn-info btn-lg blue">
       Maintenance
-      <p align="center" className="flex_item_description smaller">
-          Find the nearest garage
+      <p align="center" className="flexItemDescription smaller">
+        Find the nearest garage station
         {' '}
         <br />
         {' '}
@@ -33,13 +26,10 @@ to your location
       </p>
     </Link>
 
-    <Link
-      to="/paint-shops"
-      className="buttonstyle btn btn-success btn-lg green"
-    >
-        Paint Shops
-      <p align="center" className="flex_item_description smaller">
-          Find the nearest paint shop
+    <Link to="/paint-shops" className="buttonstyle btn btn-success btn-lg green">
+      Paint Shops
+      <p align="center" className="flexItemDescription smaller">
+        Find the nearest paint shop
         {' '}
         <br />
         {' '}
