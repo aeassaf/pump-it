6b515ec4a547c2a9e60d4cab5e6c9538be814ae7
Antoine-Assaf15 <eliasassaf.antoine@gmail.com>
2019-08-13 18:14:12 +0300

    chore: add version number and more description to about page

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -5,12 +5,15 @@ class aboutPage extends React.Component {
   state = {};
 
   render() {
+    const pjson = require('../../../package.json');
     return (
       <div className="aboutStyle flexContainer">
         The following is a website that helps you to get car services near your current location.
         <br />
         To use the app please allow your browser to have access to your current location, then
         select what you wish.
+        <br />
+        Make sure you press on a red marker on the map to get more info about a specific place
         <br />
         <br />
         I did this app during my 2019 summer internship at PostLight.
@@ -21,7 +24,11 @@ class aboutPage extends React.Component {
         If you have any feedbacks or requests please contact: eliasassaf.antoine@gmail.com
         <br />
         Special Thanks To The PostLight Team!
+        <br />
+        <br />
+        Version:
         {' '}
+        {pjson.version}
       </div>
     );
   }
