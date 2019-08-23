1bd086f62846ae76a133555d3d6f77802a28eeed
Antoine-Assaf15 <eliasassaf.antoine@gmail.com>
2019-08-23 17:33:44 +0300

    chore: adding file to solve conflict (will be deleted in few)

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import { Nav, Navbar } from 'react-bootstrap';
+import styled from 'styled-components';
+import { withRouter, Link } from 'react-router-dom';
+
+const Styles = styled.div`
+  .navbar {
+    background-color: #222;
+  }
+  .navbar-brand {
+    color: #ffffff;
+  }
+
+  .navbar-brand:hover {
+    color: orange;
+  }
+  .navbar-nav .nav-link {
+    color: #bbb;
+
+    &:hover {
+      color: white;
+    }
+  }
+`;
+
+const Navigation = () => (
+  <Styles>
+    <Navbar expand="lg">
+      <Navbar.Brand href="/">Pump It </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" className="grey" />
+
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Item>
+            <Link to="/" className="white">
+              Home
+            </Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Link to="/gas" className="white">
+              Gas
+            </Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Link to="/maintenance" className="white">
+              Maintenance
+            </Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Link to="/paint-shops" className="white">
+              Paint Shops
+            </Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Link to="/about" className="white">
+              About
+            </Link>
+          </Nav.Item>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  </Styles>
+);
+
+export default withRouter(Navigation);
